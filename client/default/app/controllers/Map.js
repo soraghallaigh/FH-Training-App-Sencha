a2c0c4cc342cfbca7f0b6259c845985b2fbe9847
Sean Legg <sean.legg@feedhenry.com>
2012-02-20 12:18:11 +0000

    Added Map Controller.

--- client/default/app/controllers/Map.js
+++ client/default/app/controllers/Map.js
@@ -1,5 +1,15 @@
 app.controllers.map = new Ext.Controller({
 
+  markers: [], // Keep track of any map markers
+
+  clearMarkers: function() {
+    if (this.markers) {
+      for (i = 0; i < this.markers.length; i++) {
+        this.markers[i].setMap(null);
+      }
+    }
+  },
+
   getLocation: function(options){
     // Instance of the google map
     var map = Ext.getCmp("map").map;
@@ -10,6 +20,13 @@ app.controllers.map = new Ext.Controller({
     }, function(res){
       pos = new google.maps.LatLng(res.lat, res.lon);
       map.setCenter(pos);
+
+      // Create a marker
+      this.markers.push(new google.maps.Marker({
+        position: pos,        
+        map: map,
+        icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=A|FF0000|000000'
+      }));  
     }, function() {
       // We failed to get the users geolocation, fallback to geo ip
       console.log("$fh.geo failed.");
