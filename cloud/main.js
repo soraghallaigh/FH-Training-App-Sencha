1594f45f589ee4934038ed464f3d48824707bad6
Sean Legg <sean.legg@feedhenry.com>
2012-02-20 13:59:22 +0000

    Work on video view.

--- cloud/main.js
+++ cloud/main.js
@@ -3,7 +3,7 @@ function getTweets() {
   //var url        = 'https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=' + username + '&count=' + num_tweets;
 
   var username   = 'feedhenry';
-  var num_tweets = 10;;
+  var num_tweets = 10;
   var url        = 'http://search.twitter.com/search.json?q=' + username;
 
   return $fh.web({
