924c5f82939f400f4ed66df980e72a42b88e6946
Sean Legg <sean.legg@feedhenry.com>
2012-02-20 12:13:50 +0000

    Added Map Controller.

--- client/default/app/controllers/Map.js
+++ client/default/app/controllers/Map.js
@@ -2,20 +2,23 @@ app.controllers.map = new Ext.Controller({
 
   getLocation: function(options){
     // Instance of the google map
-    var map = app.views.map.map;
+    var map = Ext.getCmp("map").map;
     var pos = {};
 
     $fh.geo({
       interval: 0
     }, function(res){
       pos = new google.maps.LatLng(res.lat, res.lat);
+      map.setCenter(pos);
     }, function() {
       // We failed to get the users geolocation, fallback to geo ip
       console.log("$fh.geo failed.");
 
+      /*
       $fh.geoip(function(res) { 
         console.log(JSON.stringify(res.geoip)); 
       });
+      */
     });
   }
 
