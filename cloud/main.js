abb856055f07ff98b3910aab9a0469d2e63f6be6
Sean Legg <sean.legg@feedhenry.com>
2012-02-20 11:01:29 +0000

    Tweets Page.

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,8 @@
 function getTweets() {
-  var username = 'feedhenry';
-  var url      = 'http://api.twitter.com/1/statuses/user_timeline/' + username + '.json?callback=?';
+  //var url      = 'http://api.twitter.com/1/statuses/user_timeline/' + username + '.json?callback=?';
+  var username   = 'feedhenry';
+  var num_tweets = 5;
+  var url        = 'https://api.twitter.com/1/statuses/user_timeline.json?include_entities=true&include_rts=true&screen_name=' + username + '&count=' + num_tweets;
 
   return $fh.web({
     url: url,
