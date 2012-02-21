ec85e54f22769b858a0493724d070c19a2743c1c
Sean Legg <sean.legg@feedhenry.com>
2012-02-21 09:12:57 +0000

    Work on the map view.

--- client/default/app/views/Map.js
+++ client/default/app/views/Map.js
@@ -18,7 +18,7 @@ app.views.MapView = Ext.extend(Ext.Panel, {
   layout: 'fit',
 
   listeners: {
-    pagecreate: function() {
+    onCreate: function() {
       app.views.map = new Ext.Map({
         fullscreen: true,
         layout: 'fit',
