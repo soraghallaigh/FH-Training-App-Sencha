7bbcc0b61fc5bf9654014e8579d8ac4fddc43f84
Sean Legg <sean.legg@feedhenry.com>
2012-02-21 16:29:17 +0000

    Code Refactor.

--- cloud/main.js
+++ cloud/main.js
@@ -11,8 +11,7 @@ function getTweets() {
     method: 'GET',
     allowSelfSignedCert: true
   });
-  return $fh.parse(response).body.results;
-  //return response.body.results;
+  return $fh.parse(response);
 }
 
 /*
