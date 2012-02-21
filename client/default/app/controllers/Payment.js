59fd62fac77af136cbc12f967cba74209ef709ee
Sean Legg <sean.legg@feedhenry.com>
2012-02-21 16:48:14 +0000

    Code Refactor.

--- client/default/app/controllers/Payment.js
+++ client/default/app/controllers/Payment.js
@@ -16,7 +16,7 @@ app.controllers.payment = new Ext.Controller({
       var regEx  = new RegExp("<\s*string[^>]*>(.*?)<\s*/\s*string>", "g");
       var result = regEx.exec(res.body)[1];
 
-      Ext.Msg.alert('Response'), result, Ext.emptyFn);
+      Ext.Msg.alert('Response', result, Ext.emptyFn);
     });
   }
 
